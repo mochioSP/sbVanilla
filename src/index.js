97dcff1bb68401134ce62e1dce8782bc393f26d2
keigo mochiduki <keeee50114@gmail.com>
2023-07-20 22:50:29 +0900

    アロー関数

--- src/index.js
+++ src/index.js
@@ -24,13 +24,34 @@
 /**
  * テンプレートリテラル
  */
-const name = "mochi";
-const age = 28;
+// const name = "mochi";
+// const age = 28;
 
-//従来
-const message1 = "私の名前は" + name + "です。" + age + "歳です。";
-console.log(message1);
+// //従来
+// const message1 = "私の名前は" + name + "です。" + age + "歳です。";
+// console.log(message1);
 
-//テンプレートリテラル
-const message2 = `私の名前は${name}です。${age}歳でーーーす。`;
-console.log(message2);
+// //テンプレートリテラル
+// const message2 = `私の名前は${name}です。${age}歳でーーーす。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+//従来の関数の書き方
+//
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です。"));
+
+//アロー関数で書いてみる
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です。"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
