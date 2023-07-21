918ab4250d33bc976ac3defa2b269fb762443d75
keigo mochiduki <keeee50114@gmail.com>
2023-07-21 12:55:21 +0900

    配列のコピー、結合

--- src/index.js
+++ src/index.js
@@ -84,5 +84,32 @@
 /**
  * デフォルト値、引数について
  */
-const sayHello = (name = 'ゲスト') => console.log(`こんにちは${name}さん！`);
-sayHello('nishino');
+// const sayHello = (name = 'ゲスト') => console.log(`こんにちは${name}さん！`);
+// sayHello('nishino');
+
+/**
+ * スプレッド構文...
+ */
+//配列の展開
+//const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+//まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...num3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+//配列のコピーや結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
+
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
